59e304b90a59073988e2ae5539bd662174bde239
talal823 <1098074@pdsb.net>
2026-01-14 09:56:34 -0500

    Add files via upload

---
+++ Bullet.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+
+public class Bullet {
+    private int x, y, width = 4, height = 10, speed = 10;
+
+    public Bullet(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update() { y -= speed; }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillRect(x, y, width, height);
+    }
+
+    public boolean collides(Enemy e) {
+        Rectangle r1 = new Rectangle(x, y, width, height);
+        Rectangle r2 = new Rectangle(e.getX(), e.getY(), 40, 30);
+        return r1.intersects(r2);
+    }
+
+    public int getY() { 
+        return y; 
+    }
+
+    public int getX() 
+    { 
+        return x;
+    }
+
+}
