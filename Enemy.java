bdbdcb8c68631bf3087138eee7788cbb8ec9426d
talal823 <1098074@pdsb.net>
2026-01-14 09:56:45 -0500

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+
+public class Enemy {
+    private int x;
+    private int y;
+    private int width = 40;
+    private int height = 30;
+    private int speed;
+
+    public Enemy(int x, int y, int speed) {
+        this.setX(x);
+        this.y = y;
+        this.speed = speed;
+    }
+
+    public int getX() {
+        return x;
+        
+    }
+
+    public void setX(int x) {
+        this.x = x;
+        
+    }
+
+    public void update() { y += speed; }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(getX(), y, width, height);
+    }
+
+    public boolean collides(Player p) {
+        Rectangle r1 = new Rectangle(getX(), y, width, height);
+        Rectangle r2 = new Rectangle(p.getX(), p.getY(), p.getWidth(), p.getHeight());
+        return r1.intersects(r2);
+    }
+
+    public int getY() { return y; }
+}
