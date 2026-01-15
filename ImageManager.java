7c77359ed6c4b38ec700328a11085213690e3751
talal823 <1098074@pdsb.net>
2026-01-15 17:49:38 -0500

    Update ImageManager.java

--- ImageManager.java
+++ ImageManager.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ImageManager {
-    // For simplicity, currently we use colors in draw()
-    // You can load images later:
     public static Image load(String path) {
         return new ImageIcon(path).getImage();
     }
