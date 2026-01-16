894305d81f137447abf5ddaa67110d04d5d62212
talal823 <1098074@pdsb.net>
2026-01-15 19:31:43 -0500

    Update BackgroundManager.java

--- BackgroundManager.java
+++ BackgroundManager.java
@@ -2,18 +2,17 @@ import java.awt.*;
 import java.util.Random;
 
 public class BackgroundManager {
+
     private int type;
     private Random random = new Random();
 
     public BackgroundManager() {
-        type = random.nextInt(3); // 0,1,2
+        type = random.nextInt(1); // expand later
     }
 
     public void draw(Graphics g, int width, int height) {
         switch (type) {
-            case 0: g.setColor(Color.BLACK); break;
-            //case 1: g.setColor(Color.DARK_GRAY); break;
-            //case 2: g.setColor(Color.BLUE); break;
+            case 0 -> g.setColor(Color.BLACK);
         }
         g.fillRect(0, 0, width, height);
     }
