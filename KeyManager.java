29037c5c7fefd79e101f6a21f6e39a27e2338373
talal823 <1098074@pdsb.net>
2026-01-18 22:10:30 -0500

    Update KeyManager.java

--- KeyManager.java
+++ KeyManager.java
@@ -1,8 +1,14 @@
-import java.awt.event.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class KeyManager implements KeyListener {
 
     private final GameLogic logic;
+
+    // track which keys are currently pressed
+    private boolean leftPressed = false;
+    private boolean rightPressed = false;
+    private boolean spacePressed = false;
 
     public KeyManager(GameLogic logic) {
         this.logic = logic;
@@ -11,15 +17,31 @@ public class KeyManager implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_LEFT -> logic.moveLeft();
-            case KeyEvent.VK_RIGHT -> logic.moveRight();
-            case KeyEvent.VK_SPACE -> logic.shoot();
+            case KeyEvent.VK_LEFT -> leftPressed = true;
+            case KeyEvent.VK_RIGHT -> rightPressed = true;
+            case KeyEvent.VK_SPACE -> spacePressed = true;
             case KeyEvent.VK_R -> {
                 if (logic.isGameOver()) logic.reset();
             }
         }
     }
 
-    public void keyReleased(KeyEvent e) {}
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_LEFT -> leftPressed = false;
+            case KeyEvent.VK_RIGHT -> rightPressed = false;
+            case KeyEvent.VK_SPACE -> spacePressed = false;
+        }
+    }
+
+    @Override
     public void keyTyped(KeyEvent e) {}
+
+    
+    public void update() {
+        if (leftPressed) logic.moveLeft();
+        if (rightPressed) logic.moveRight();
+        if (spacePressed) logic.shoot();
+    }
 }
